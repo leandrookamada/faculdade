bc034d64940b9ba73a06f29a840b63bb064b4894
leandrookamada <leandrolobatokmd@gmail.com>
2025-07-02 20:08:22 -0300

    03

--- Algoritmo-1/Trabalho segundo bimestre/ATIVIDADE02.cpp
+++ Algoritmo-1/Trabalho segundo bimestre/ATIVIDADE02.cpp
@@ -1,9 +1,18 @@
 #include<stdio.h>
 #include<math.h>
 
-bool verificaSeEhPrimo(int num){
-    if(num == 1){
+// Questão 02 - Leia 10 números inteiros e armazene-os em um vetor.
+// Em seguida, identifique e  exiba os elementos que são números primos, juntamente com suas 
+// respectivas posições no vetor.
+
+bool verificaSeEhPrimo(int num[10]){
+    for(int i = 0; i < 10; i++){
         
     }
     return true;
+}
+int main(){
+    int num[10];
+
+    return 0;
 }
