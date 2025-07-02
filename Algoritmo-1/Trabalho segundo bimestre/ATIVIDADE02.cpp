65325301e3cff675afb5932ffc65b35d14a51688
leandrookamada <leandrolobatokmd@gmail.com>
2025-07-02 20:45:13 -0300

    04

--- Algoritmo-1/Trabalho segundo bimestre/ATIVIDADE02.cpp
+++ Algoritmo-1/Trabalho segundo bimestre/ATIVIDADE02.cpp
@@ -5,11 +5,17 @@
 // Em seguida, identifique e  exiba os elementos que são números primos, juntamente com suas 
 // respectivas posições no vetor.
 
-bool verificaSeEhPrimo(int num[10]){
+int verificaSeEhPrimo(int NUMBER[10]){
+    int incremento = 0;
+    
     for(int i = 0; i < 10; i++){
-        
+        if(NUMBER[i] % 2 == 0 || NUMBER[i] % 3 == 0 || NUMBER[i] % 5 == 0 || NUMBER[i] % 7 == 0 || NUMBER[i] % 9 == 0){
+            if(NUMBER[i] == 1 || NUMBER[i] == 2 || NUMBER[i] == 3 || NUMBER[i] == 5 || NUMBER[i] == 7 || NUMBER[i] == 9 ){
+                
+            }
+        }
     }
-    return true;
+    return 0;
 }
 int main(){
     int num[10];
