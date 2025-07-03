59da5ac82f629040ce6a640c51e369e14c77f400
leandrookamada <leandrolobatokmd@gmail.com>
2025-07-02 21:38:05 -0300

    o6

--- Algoritmo-1/Trabalho segundo bimestre/ATIVIDADE01.cpp
+++ Algoritmo-1/Trabalho segundo bimestre/ATIVIDADE01.cpp
@@ -1,7 +1,7 @@
 #include<stdio.h>
 #include<math.h>
 
-float calculaAMedia(float A[15]){
+float calculaAMedia(int A[15]){
     float media;
     for(int i = 0; i < 15; i++){
         media += A[i];
@@ -9,26 +9,41 @@ float calculaAMedia(float A[15]){
     
     return media/15;
 }
-void calculaMaioreMenor(float A[15], float *maior, float *menor) {
-    *maior = A[0];
-    *menor = A[0];
-    for (int i = 1; i < 15; i++) {
-        if (*maior < A[i]) *maior = A[i];
-        if (*menor > A[i]) *menor = A[i];
+
+int calculaMaior(int A[15]){
+    int maior = 0;
+    for (int i = 0; i < 15; i++){
+        if(maior < A[i]){
+            maior = A[i];
+        }
     }
+    return maior;
 }
+
+int calculaMenor(int A[15]){
+    int menor = 100;
+    for (int i = 0; i < 15; i++){
+        if(menor > A[i]){
+            menor = A[i];
+        }
+    }
+    return menor;
+}
+
 int main(){
-    float media, maior, menor, A[15];
+    int maior, menor, A[15];
+    float media;
     for (int i = 0; i < 15; i++){
         printf("\nPor favor, insira a nota do %d aluno: ", i+1);
-        scanf("%f", &A[i]);
+        scanf("%d", &A[i]);
     }
     media = calculaAMedia(A);
-    calculaMaioreMenor(A, &maior, &menor);
+    maior = calculaMaior(A);
+    menor = calculaMenor(A);
 
     printf("\nA media da turma é: %.1f\n", media);
-    printf("\nA maior nota da turma foi: %.1f\n", maior);
-    printf("\nA menor nota da turma foi: %.1f\n", menor);
+    printf("\nA maior nota da turma foi: %d\n", maior);
+    printf("\nA menor nota da turma foi: %d\n", menor);
 
     return 0;
     
