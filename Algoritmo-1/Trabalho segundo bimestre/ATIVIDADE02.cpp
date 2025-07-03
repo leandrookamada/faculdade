8d958b5d3c65559eaaa80f755867779feb93a830
leandrookamada <leandrolobatokmd@gmail.com>
2025-07-02 21:28:29 -0300

    05

--- Algoritmo-1/Trabalho segundo bimestre/ATIVIDADE02.cpp
+++ Algoritmo-1/Trabalho segundo bimestre/ATIVIDADE02.cpp
@@ -1,24 +1,41 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdbool.h>
 
 // Questão 02 - Leia 10 números inteiros e armazene-os em um vetor.
 // Em seguida, identifique e  exiba os elementos que são números primos, juntamente com suas 
 // respectivas posições no vetor.
 
-int verificaSeEhPrimo(int NUMBER[10]){
-    int incremento = 0;
-    
-    for(int i = 0; i < 10; i++){
-        if(NUMBER[i] % 2 == 0 || NUMBER[i] % 3 == 0 || NUMBER[i] % 5 == 0 || NUMBER[i] % 7 == 0 || NUMBER[i] % 9 == 0){
-            if(NUMBER[i] == 1 || NUMBER[i] == 2 || NUMBER[i] == 3 || NUMBER[i] == 5 || NUMBER[i] == 7 || NUMBER[i] == 9 ){
-                
-            }
+bool ehPrimo(int num) {
+    if (num < 1) {
+        return false;
+    }
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return false;
         }
     }
-    return 0;
+    return true;
 }
 int main(){
     int num[10];
+    int incremento = 0;
 
+    for(int i = 0; i < 10; i++){
+        printf("\nPor favor, insira o primeiro número: ");
+        scanf("%d", &num[i]);
+    }
+    for(int i = 0; i < 10; i++){
+        if(ehPrimo(num[i])){
+            incremento ++;
+        }
+    }
+
+    printf("\n Dos valores inseridos, %d são primos e eles estão nas posições:", incremento);
+    for( int i = 0; i < 10; i++){
+        if(ehPrimo(num[i])){
+            printf(" \n %d", i);
+        }
+    }
     return 0;
 }
